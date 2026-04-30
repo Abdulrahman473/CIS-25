dd61da8363660efd6c3772cdced7c5a858662490
Abdulrahman473 <ahmedabdulrahman473@gmail.com>
2026-05-01 02:27:01 +0400

    Update Task.h

--- TO-DO List Project/Task.h
+++ TO-DO List Project/Task.h
@@ -1,1 +1,18 @@
+#ifndef TASK_H
+#define TASK_H
 
+#include <string>
+#include <vector>
+using namespace std;
+
+// Task structure
+struct Task {
+    string name;
+    bool completed;
+};
+
+// Function declarations
+void displayTasks(const vector<Task>& tasks);
+void addTask(vector<Task>& tasks);
+void completeTask(vector<Task>& tasks);
+void deleteTask(vector<Task>& tasks);
